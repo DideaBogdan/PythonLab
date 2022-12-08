304c31de6625ac5d9012b47e29f39f990e264739
DideaBogdan <dideabogdan@gmail.com>
2022-12-08 17:27:49 +0200

    One function for encrypt/decrypt, eliminated .encrypted extension with flag in file

--- ProiectC/FileCryptor.py
+++ ProiectC/FileCryptor.py
@@ -10,11 +10,6 @@ def validate_argv():
         raise Exception(f"Error: There is no such file at {sys.argv[2]}...")
     if os.path.isfile(sys.argv[2]) is False:
         raise Exception(f"Error: The {sys.argv[2]} is not a file!")
-    if sys.argv[1] == 'encrypt' and os.path.splitext(sys.argv[2])[1] == '.encrypted':  #eroare pentru încercare de criptare pe fisier deja criptat
-        raise Exception("Error: File is already encrypted!")
-    if sys.argv[1] == 'decrypt' and os.path.splitext(sys.argv[2])[1] != '.encrypted':  #eroare pentru încercare de decriptare pe fisier care nu este criptat
-        raise Exception("Error: This file is not encrypted!")
-
 
 def hashSHA256(fl):
     """
@@ -32,64 +27,62 @@ def hashSHA256(fl):
     return m.hexdigest()
 
 
-def encrypt():
+def crypt(encrypt):
     """
     Cand se criptează se generează hash-ul fișierului, apoi se citește din fisier caracter cu caracter,
     pentru fiecare se aduna codul ASCII cu cel al caracterului din iteratia curenta a parolei
     Apoi se pune hash-ul pe 64 de caractere si sirul criptat in noul fisier creat ".encrypted".
 
     """
-    hsh = hashSHA256(sys.argv[2])
+    if encrypt is True:
+        hsh = hashSHA256(sys.argv[2])
     password = sys.argv[3]
+    h = hashlib.sha256()
+    h.update(password.encode('ascii'))
+    hshpass = h.hexdigest()
     enc = str()
     ind_pass = 0
     with open(sys.argv[2], "r", encoding='utf-8') as f:
+        if encrypt is False:
+            check = f.read(6)
+            if check != 'Bogdan':
+                raise Exception("Error: This file is not encrypted!")
+            verify_pass = f.read(64)
+            if verify_pass != hshpass:
+                raise Exception("Password was wrong! File couldn't be decrypted!Retry...")
+            hsh = f.read(64)
+        already_encrypted = f.read(6)
+        if encrypt is True and already_encrypted == 'Bogdan':
+            raise Exception("Error: File is already encrypted!")
+        if encrypt is False:
+            f.seek(134)
+        else:
+            f.seek(0)
         byte = f.read(1)
         while byte != '':
-            enc += chr((ord(byte) + ord(password[ind_pass])) % 256)
+            if encrypt is True:
+                enc += chr((ord(byte) + ord(password[ind_pass])) % 256)
+            else:
+                enc += chr(((ord(byte) - ord(password[ind_pass]) + 256) % 256))
             ind_pass += 1
             if ind_pass == len(password):
                 ind_pass = 0
             byte = f.read(1)
-    with open(sys.argv[2] + ".encrypted", "w", encoding='utf-8')as f:
-        f.write(hsh)
-        f.write(enc)
-    os.remove(sys.argv[2])
+    if encrypt is True:
+        with open(sys.argv[2], "w", encoding='utf-8')as f:
+            f.write("Bogdan")
+            f.write(hshpass)
+            f.write(hsh)
+            f.write(enc)
+    else:
+        with open(sys.argv[2], "w", encoding='utf-8') as f:
+            f.write(enc)
 
-
-def decrypt():
-    """
-    Se ia fișierul criptat din care se citește si se fac operatii pe fiecare caracter similar cu functia de criptare,
-    doar ca se scad valorile ASCII. Primii 64 de octeti sunt citiți odata, deoarece reprezinta hash-ul fișierului
-    decriptat. Dupa ce se decriptează, se creaza hash pe sirul decriptat si se compara cu cel al fișierului initial.
-    Daca hash-urile nu se potrivesc înseamna ca parola a fost greșita si se anulează operatia de decriptare. Altfel
-    se va crea fișierul decriptat.
-    """
-    password = sys.argv[3]
-    dec = str()
-    ind_pass = 0
-    with open(sys.argv[2], "r", encoding='utf-8') as f:
-        hsh = f.read(64)
-        byte = f.read(1)
-        while byte != '':
-            dec += chr(((ord(byte) - ord(password[ind_pass])+256) % 256))
-            ind_pass += 1
-            if ind_pass == len(password):
-                ind_pass = 0
-            byte = f.read(1)
-    newfile = os.path.splitext(sys.argv[2])[0]
-    with open(newfile, "w", encoding='utf-8') as f:
-        f.write(dec)
-    checkhash = hashSHA256(newfile)
-    if checkhash != hsh:
-        os.remove(newfile)
-        raise Exception("Password was wrong! File couldn't be decrypted!Retry...")
-    os.remove(sys.argv[2])
 
 
 if __name__ == '__main__':
     validate_argv()
     if sys.argv[1] == "encrypt":
-        encrypt()
+        crypt(True)
     elif sys.argv[1] == "decrypt":
-        decrypt()
+        crypt(False)
